ed0e58c2557b7671bd8a356533215061ed9fadb9
pavanulichi <pavan.ulichi@gmail.com>
2017-09-12 16:52:43 -0400

    bot

---
+++ bot.js
@@ -0,0 +1,261 @@
+//
+// This is main file containing code implementing the Express server and functionality for the Express echo bot.
+//
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+require('dotenv').config();
+var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
+
+// The rest of the code implements the routes for our Express server.
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Webhook validation
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+    console.log("Validating webhook howdy");
+    res.status(200).send(req.query['hub.challenge']);
+
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+// Display the web page
+app.get('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(messengerButton);
+  res.end();
+});
+
+// Message processing
+app.post('/webhook', function (req, res) {
+  console.log(req.body);
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else if (event.postback) {
+          receivedPostback(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+// Incoming events handling
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the template example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+      case 'show':
+        callRrApi(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+function callRrApi(sid){
+  request({
+    uri: 'https://recs.richrelevance.com/rrserver/api/rrPlatform/recsForPlacements',
+    qs: { apiKey: process.env.API_KEY,
+          apiClientKey: process.env.API_CLIENT_KEY,
+          userId: process.env.USER_ID,
+          sessionId: process.env.SESSION_ID,
+          placements: process.env.PLACEMENTS_ID},
+          headers: {
+            'User-Agent': 'Super Agent/0.0.1'
+          },
+          method: 'GET',
+
+        }, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log('Google log start golden');
+            console.log(body) // Print the google web page.
+            console.log('Google log end golden');
+            sendTextMessage(sid, 'Pavan check logs');
+          } else {
+            console.log('Google log start golden');
+            console.log(body) // Print the google web page.
+            console.log('Google log end golden');
+            sendTextMessage(sid, 'Pavan, ERROR');
+          }
+        });
+        //   var request = require('request');
+        //   request('https://recs.richrelevance.com/rrserver/api/rrPlatform/recsForPlacements?apiKey=showcaseparent&apiClientKey=85e8da71946f5bdd&userId=1&sessionId=1&placements=generic_page.panda_test', function (error, response, body) {
+        //   if (!error && response.statusCode == 200) {
+        //     console.log('Google log start boomer');
+        //     console.log(body) // Print the google web page.
+        //     console.log('Google log end boomer');
+        //     sendTextMessage(sid, 'Pavan check logs');
+        //   }
+        //   else {
+        //     console.log('Google log start boomer');
+        //     console.log(body) // Print the google web page.
+        //     console.log('Google log end boomer');
+        //     sendTextMessage(sid, 'Pavan, ERROR');
+        //   }
+        // });
+
+      }
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback
+  // button for Structured Messages.
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to
+  // let them know it was successful
+  sendGenericMessage(senderID);
+}
+
+//////////////////////////
+// Sending helpers
+//////////////////////////
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",
+            image_url: "https://external-lga3-1.xx.fbcdn.net/safe_image.php?d=AQDUUW6fimr9ZuPQ&url=https%3A%2F%2Fscontent.oculuscdn.com%2Fv%2Ft64.5771-25%2F12727726_260257514396959_4582648530518147072_n.jpg%3Foh%3Da26dd5f6c8434607575d39b64c12049f%26oe%3D59E3594A&_nc_hash=AQCP2Nl7pLxh5Sdn",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",
+            image_url: "https://external-lga3-1.xx.fbcdn.net/safe_image.php?d=AQBJI4Fdao_BXpGf&url=https%3A%2F%2Fscontent.oculuscdn.com%2Fv%2Ft64.5771-25%2F12533983_274907789592476_6510796530947981312_n.jpg%3Foh%3Db47e1e9e40864af371d816ccdd985e00%26oe%3D59E489A3&_nc_hash=AQDW9ho0dOMMtK3I",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  console.log("Dunkirk start");
+  console.log(process.env.PAGE_ACCESS_TOKEN);
+  console.log(process.env.VERIFY_TOKEN);
+  console.log("Dunkirk end");
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+// Set Express to listen out for HTTP requests
+var server = app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port %s", server.address().port);
+});
