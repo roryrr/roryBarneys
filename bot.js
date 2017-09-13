75a271e9761ac49f8a87cd6a7946d054755e1809
pavanulichi <pavan.ulichi@gmail.com>
2017-09-13 16:16:26 -0400

    json

--- bot.js
+++ bot.js
@@ -8,7 +8,8 @@ const request = require('request');
 const path = require('path');
 require('dotenv').config();
 var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
-
+//Declaring variables that store data from the response of RR API
+//var rr_array = [];
 // The rest of the code implements the routes for our Express server.
 let app = express();
 
@@ -123,7 +124,7 @@ function callRrApi(sid){
             body = JSON.parse(body);
 
             // The Description is:  "descriptive string"
-            console.log("Got a response mummy: ", body.rcs);
+            console.log("Got a response scofield: ", body.placements[0].recommendedProducts[0].clickURL);
             sendTextMessage(sid, 'Pavan check logs');
           } else {
             console.log('Google log start golden');
