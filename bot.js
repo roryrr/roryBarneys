f60b5d1ca8fcf78f88b11f52d9a063e4734e9a20
pavanulichi <pavan.ulichi@gmail.com>
2017-09-19 16:57:35 -0400

    image

--- bot.js
+++ bot.js
@@ -324,7 +324,7 @@ function sendGenericMessage(recipientId) {
   var itemList = [];
 rr_array.forEach(i=>{
   console.log('ranbir');
-  console.log(cloudinary.url(i.imageURL,{ type: 'fetch', width: 170, height: 170, crop: 'fill', fetch_format: 'jpg' }));
+  console.log(cloudinary.url(i.imageURL,{ type: 'fetch', width: 170, height: 170, crop: 'fit', fetch_format: 'jpg' }));
   console.log('ranbir');
    itemList.push(
    {
@@ -332,7 +332,7 @@ rr_array.forEach(i=>{
     "subtitle":i.brand,
     "item_url":i.productURL,
     //manipulating the image using Cloudinary
-    "image_url":cloudinary.url(i.imageURL,{ type: 'fetch', width: 170, height: 170, crop: 'fill', fetch_format: 'jpg' }),
+    "image_url":cloudinary.url(i.imageURL,{ type: 'fetch', width: 170, height: 170, crop: 'fit', fetch_format: 'jpg' }),
     "buttons" : [
       {
           "type": "web_url",
