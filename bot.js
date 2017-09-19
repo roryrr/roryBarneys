74a699e0333889d0c4468d4199369d2ac657b7c4
pavanulichi <pavan.ulichi@gmail.com>
2017-09-19 17:02:35 -0400

    image

--- bot.js
+++ bot.js
@@ -332,7 +332,7 @@ rr_array.forEach(i=>{
     "subtitle":i.brand,
     "item_url":i.productURL,
     //manipulating the image using Cloudinary
-    "image_url":cloudinary.url(i.imageURL,{ type: 'fetch', width: 170, height: 170, crop: 'fit', fetch_format: 'jpg' }),
+    "image_url":cloudinary.url(i.imageURL,{ type: 'fetch', width: 150, height: 150, crop: 'fit', fetch_format: 'jpg' }),
     "buttons" : [
       {
           "type": "web_url",
@@ -361,7 +361,7 @@ console.log(itemList);
         type: "template",
         payload: {
           template_type: "generic",
-          image_aspect_ratio: "square",
+          // image_aspect_ratio: "square",
           elements: itemList
         }
       }
