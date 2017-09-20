35f06b632de94c9e7d772a0a7560c909fc70af9e
pavanulichi <pavan.ulichi@gmail.com>
2017-09-20 09:25:35 -0400

    square

--- bot.js
+++ bot.js
@@ -332,7 +332,7 @@ rr_array.forEach(i=>{
     "subtitle":i.brand,
     "item_url":i.productURL,
     //manipulating the image using Cloudinary
-    "image_url":cloudinary.url(i.imageURL,{ type: 'fetch', height: 150, crop: 'scale', quality: 100, fetch_format: 'jpg'}),
+    "image_url":cloudinary.url(i.imageURL,{ type: 'fetch', height: 170, width: 170, crop: 'scale', quality: 100, fetch_format: 'jpg'}),
     "buttons" : [
       {
           "type": "web_url",
