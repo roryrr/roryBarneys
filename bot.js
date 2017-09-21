4d37dc0a1471d14b52501091ee9422b0379d2e8b
pavanulichi <pavan.ulichi@gmail.com>
2017-09-21 18:06:56 -0400

    new1

--- bot.js
+++ bot.js
@@ -541,10 +541,10 @@ function dataBuilder(d, myArray){
             }
             console.log("Deepika start");
             //console.log(rr_array);
-            // sendTextMessage(d, 'Hiii');
+            sendGenericMessage(d, rr_array_fav);
           });
     });
-    sendGenericMessage(d, rr_array_fav);
+    // sendGenericMessage(d, rr_array_fav);
 }
 
 function callSendAPI(messageData) {
