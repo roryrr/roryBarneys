6c940d2de7021aca16ffd2ead6f63dd479465d91
pavanulichi <pavan.ulichi@gmail.com>
2017-09-25 14:40:30 -0400

    hee

--- bot.js
+++ bot.js
@@ -105,47 +105,47 @@
 
       let apiai = apiaiApp.textRequest(messageText, {
           sessionId: 'tabby_cat' // use any arbitrary id
-
         });
+        console.log("aparichitudu" + apiai.body.result.parameters['geo-city']);
 
-        apiai.on('response', (response) => {
-          // Got a response from api.ai. Let's POST to Facebook Messenger
-          let aiText = response.result.fulfillment.speech;
-          console.log("kane wwe" + response.result.metadata.intentName);
-          if (response.result.metadata.intentName === 'weather intent') {
-            let city = req.body.result.parameters['geo-city'];
-            let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
-            console.log("inside weather api pavan");
-
-            request.get(restUrl, (err, response, body) => {
-              if (!err && response.statusCode == 200) {
-                let json = JSON.parse(body);
-                let msg = json.weather[0].description + ' and the temperature is ' + json.main.temp + ' ℉';
-                return res.json({
-                  speech: msg,
-                  displayText: msg,
-                  source: 'weather'});
-            }else {
-              return res.status(400).json({
-                status: {
-                  code: 400,
-                  errorType: 'I failed to look up the city name.'}});
-                }
-          });
-        }
-            else {
-              var messageData = {
-                recipient: {
-                  id: senderID
-                },
-                message: {
-                  text: aiText
-                }
-              };
-
-          callSendAPI(messageData);
-        }
-        });
+        // apiai.on('response', (response) => {
+        //   // Got a response from api.ai. Let's POST to Facebook Messenger
+        //   let aiText = response.result.fulfillment.speech;
+        //   console.log("kane wwe" + response.result.metadata.intentName);
+        //   if (response.result.metadata.intentName === 'weather intent') {
+        //     let city = req.body.result.parameters['geo-city'];
+        //     let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
+        //     console.log("inside weather api pavan");
+        //
+        //     request.get(restUrl, (err, response, body) => {
+        //       if (!err && response.statusCode == 200) {
+        //         let json = JSON.parse(body);
+        //         let msg = json.weather[0].description + ' and the temperature is ' + json.main.temp + ' ℉';
+        //         return res.json({
+        //           speech: msg,
+        //           displayText: msg,
+        //           source: 'weather'});
+        //     }else {
+        //       return res.status(400).json({
+        //         status: {
+        //           code: 400,
+        //           errorType: 'I failed to look up the city name.'}});
+        //         }
+        //   });
+        // }
+        //     else {
+        //       var messageData = {
+        //         recipient: {
+        //           id: senderID
+        //         },
+        //         message: {
+        //           text: aiText
+        //         }
+        //       };
+        //
+        //   callSendAPI(messageData);
+        // }
+        // });
 
         apiai.on('error', (error) => {
           console.log("Pikachu" + error);
