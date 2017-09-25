ea61edf69d866fb3b8ae536585c37b4395ec72b5
pavanulichi <pavan.ulichi@gmail.com>
2017-09-25 15:23:21 -0400

    weather

--- bot.js
+++ bot.js
@@ -82,28 +82,40 @@ app.post('/webhook', function (req, res) {
     res.sendStatus(200);
   }
 });
-
-
+/* Webhook for API.ai to get response from the 3rd party API */
 app.post('/ai', (req, res) => {
+  console.log('*** Webhook for api.ai query ***');
+  console.log(req.body.result);
+
   if (req.body.result.action === 'weather') {
+    console.log('*** weather ***');
     let city = req.body.result.parameters['geo-city'];
     let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
 
     request.get(restUrl, (err, response, body) => {
       if (!err && response.statusCode == 200) {
         let json = JSON.parse(body);
-        let msg = json.weather[0].description + ' and the temperature is ' + json.main.temp + ' ℉';
+        console.log(json);
+        let tempF = ~~(json.main.temp * 9/5 - 459.67);
+        let tempC = ~~(json.main.temp - 273.15);
+        let msg = 'The current condition in ' + json.name + ' is ' + json.weather[0].description + ' and the temperature is ' + tempF + ' ℉ (' +tempC+ ' ℃).'
         return res.json({
           speech: msg,
           displayText: msg,
-          source: 'weather'});
+          source: 'weather'
+        });
       } else {
+        let errorMessage = 'I failed to look up the city name.';
         return res.status(400).json({
           status: {
             code: 400,
-            errorType: 'I failed to look up the city name.'}});
-      }})
+            errorType: errorMessage
+          }
+        });
+      }
+    })
   }
+
 });
 
 // Incoming events handling (this handles both user text input and also text from payload that comes from FB api)
