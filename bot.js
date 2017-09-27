f4d8fc238e491da800d7fcf8f9219d215b43ed98
pavanulichi <pavan.ulichi@gmail.com>
2017-09-26 21:20:09 -0400

    image dimension

--- bot.js
+++ bot.js
@@ -560,7 +560,7 @@ arrayHere.forEach(i=>{
     "subtitle":i.brand,
     "item_url":process.env.BNY_HOME + i.linkId,
     //manipulating the image using Cloudinary
-    "image_url":cloudinary.url(i.imageId,{ type: 'fetch', height: 170, crop: 'scale', quality: 100, fetch_format: 'jpg'}),
+    "image_url":cloudinary.url(i.imageId,{ type: 'fetch', height: 170, crop: 'fit', quality: 100, fetch_format: 'jpg'}),
     "buttons" : [
         {
           "type": "postback",
