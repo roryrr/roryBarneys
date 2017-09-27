5ab84fa8c9bbc279ce0e4bc75cf1fb25c1a4aea2
pavanulichi <pavan.ulichi@gmail.com>
2017-09-26 21:32:40 -0400

    image dimension

--- bot.js
+++ bot.js
@@ -516,7 +516,7 @@ arrayHere.forEach(i=>{
     "subtitle":i.brand,
     "item_url":i.productURL,
     //manipulating the image using Cloudinary
-    "image_url":cloudinary.url(i.imageURL,{ type: 'fetch', height: 120, crop: 'pad', quality: 100, fetch_format: 'jpg'}),
+    "image_url":cloudinary.url(i.imageURL,{ type: 'fetch', height: 170, width: 170, crop: 'scale', quality: 100, fetch_format: 'jpg'}),
     "buttons" : [
         {
           "type": "postback",
@@ -560,7 +560,7 @@ arrayHere.forEach(i=>{
     "subtitle":i.brand,
     "item_url":process.env.BNY_HOME + i.linkId,
     //manipulating the image using Cloudinary
-    "image_url":cloudinary.url(i.imageId,{ type: 'fetch', height: 120, crop: 'pad', quality: 100, fetch_format: 'jpg'}),
+    "image_url":cloudinary.url(i.imageId,{ type: 'fetch', height: 170, width: 170, crop: 'scale', quality: 100, fetch_format: 'jpg'}),
     "buttons" : [
         {
           "type": "postback",
