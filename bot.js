c6c9cc1144289f30b1e09d998d39f34baaab13c1
pavanulichi <pavan.ulichi@gmail.com>
2017-09-29 12:07:00 -0400

    you choose

--- bot.js
+++ bot.js
@@ -301,6 +301,9 @@ function receivedMessage(event) {
       else if (derivedPayload == "v2_brand") {
         sendTextMessage(senderID, "Type in a brand you're looking for");
       }
+      else if (derivedPayload == "v2_youChoose") {
+        callRrApi(senderID, "default");
+      }
       else if (derivedPayload.match(/(v2_BNY-)/g)) {
         callRrApi(senderID, derivedPayload.slice(3));
       }
