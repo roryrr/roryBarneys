e89ea55e78ad32c05a42348b64e2ba2a7b0a50a8
pavanulichi <pavan.ulichi@gmail.com>
2017-09-28 20:21:35 -0400

    just looking

--- bot.js
+++ bot.js
@@ -284,7 +284,7 @@ function receivedMessage(event) {
   if (messageText) {
     // If we receive a text message, check to see if it matches a keyword
     // and send back the template example. Otherwise, just echo the text we received.
-    if(message.quick_reply && ((messageText == "See favorites") || (messageText == "Just looking") || (messageText == "Find it") || (messageText == "Category") || (messageText == "Brand") || (messageText == "You choose"))){
+    if(message.quick_reply && (message.quick_reply["payload"]).match(/(v2_)/g)){
       var derivedPayload = message.quick_reply["payload"];
       if (derivedPayload == "v2_find") {
         v2_showFindList(senderID);
