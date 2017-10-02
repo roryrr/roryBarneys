4940b411c16d32df69d5a12a50d7d699a6ca5da5
pavanulichi <pavan.ulichi@gmail.com>
2017-10-02 11:55:59 -0400

    inspire color filter

--- bot.js
+++ bot.js
@@ -269,6 +269,7 @@ app.post('/ai', (req, res) => {
     findProductName.replace(" ", "+");
     var findcolor = req.body.result.parameters['color'];
     findcolor = 'color:\"' + findcolor + '\"';
+    console.log(findcolor);
     var findSize = req.body.result.parameters['size'];
     findSize.replace(" ", "+");
     var findPrice = req.body.result.parameters['unit-currency'];
