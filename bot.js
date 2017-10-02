4464b1d024e63cbef532f86abd249c3d5e0d4616
pavanulichi <pavan.ulichi@gmail.com>
2017-10-02 16:17:35 -0400

    inspire color filter

--- bot.js
+++ bot.js
@@ -290,7 +290,7 @@ app.post('/ai', (req, res) => {
           query: findProductName,
           start: "0",
           rows: "5",
-          filter : [findBrand, findGender]
+          filter : {findBrand, findGender}
           };
         request({
           uri: req_url,
