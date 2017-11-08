53281991e92ec65e90e29c01db914f5751a3fc46
pavanulichi <pavan.ulichi@gmail.com>
2017-11-08 09:45:43 -0500

    qr

--- bot.js
+++ bot.js
@@ -559,6 +559,7 @@
             callRrApi(senderID, "default");
           }
           else if (derivedPayload == "v2_restart") {
+            GLOBAL_PRODUCT_GENDER="", GLOBAL_PRODUCT_COLOR="", GLOBAL_PRODUCT_NAME="", GLOBAL_PRODUCT_BRAND="", GLOBAL_PRODUCT_SIZE="";
             v2_initialOptions(senderID);
           }
           else if (derivedPayload == "v2_tips") {
@@ -751,42 +752,32 @@
         facet = 'size';
       }
 
-
       var req_url = process.env.FIND_URL;
-      var queryParameters = { apiKey: process.env.API_KEY,
-            apiClientKey: process.env.API_CLIENT_KEY,
-            userId: process.env.USER_ID,
-            sessionId: process.env.SESSION_ID,
-            placements: process.env.PLACEMENTS_ID_FIND,
-            lang: "en",
-            query: pName,
-            facet: facet,
-            start: 0,
-            rows: "9"};
-          request({
-            uri: req_url,
-            qs: queryParameters,
-            headers: {
-              'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; A1 Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Mobile Safari/537.36'
-              },
-            method: 'GET',
-            }, function (error, response, body) {
-                  if (!error && response.statusCode == 200) {
-                    //parsing the json response from RR cloud
-                    body = JSON.parse(body);
-                    console.log("powerranger");
-                    facet_array = body.placements[0].facets[0].values;
-                    sendFacetOptions(sid, facet_array.slice(0,8), pName, facet);
-                    console.log(facet_array);
-                          // setTimeout(function() { v2_restartAnytime(GLOBAL_ID) }, 7000);
-                // The Description is:  "descriptive string"
-              } else {
-              console.log('Pavan api.ai, ERROR');
-              }
-            });
+      var apiKey= process.env.API_KEY,
+            apiClientKey= process.env.API_CLIENT_KEY,
+            userId= process.env.USER_ID,
+            sessionId= process.env.SESSION_ID,
+            placements= process.env.PLACEMENTS_ID_FIND,
+            lang= "en",
+            query= pName,
+            facet= facet,
+            start= 0,
+            rows= "9";
+      var  requesting = req_url + "?apiKey=" + apiKey + "&apiClientKey=" + apiClientKey + "&userId=" + userId + "&sessionId=" + sessionId + "&placements=" + placements + "&lang=en&start=0&rows=9&query=" + query + "&filter=" + GLOBAL_PRODUCT_BRAND + "&filter=" + GLOBAL_PRODUCT_GENDER + "&filter=" + GLOBAL_PRODUCT_COLOR + "&filter=" + GLOBAL_PRODUCT_SIZE + "&facet=" + facet;
+        request(requesting, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            //parsing the json response from RR cloud
+            body = JSON.parse(body);
+            console.log("powerranger");
+            facet_array = body.placements[0].facets[0].values;
+            sendFacetOptions(sid, facet_array.slice(0,8), pName, facet);
+                  // setTimeout(function() { v2_restartAnytime(GLOBAL_ID) }, 7000);
+        // The Description is:  "descriptive string"
+        } else {
+            console.log('Pavan api.ai, ERROR');
+        }
+    });
 
-
-    }
 
     function sendFacetOptions(recipientId, arrayHere, pName, facet){
       if (arrayHere.length !== 0) {
