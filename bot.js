83d953316e6c1927c11e7f4b48c3aa81a3cc1e3d
pavanulichi <pavan.ulichi@gmail.com>
2017-11-20 14:35:23 -0500

    mark seen

--- bot.js
+++ bot.js
@@ -1546,6 +1546,9 @@
       request({
         uri: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        headers: {
+          'Content-Type': 'application/json'
+          },
         method: 'POST',
         json: messageData
 
